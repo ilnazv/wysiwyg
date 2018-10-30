796efc0a6adaf9d5c738ba3e6edcc2a3310ecf33
Ilnaz Valiev <ilnazv90@gmail.com>
2018-10-29 22:17:57 -0700

    ng 7, flex, etc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,51 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { RouterModule, Routes } from '@angular/router';
+import {
+  MatToolbarModule, MatButtonModule,
+  MatButtonToggleModule,
+  MatSidenavModule, MatIconModule,
+  MatListModule, MatGridListModule,
+  MatCardModule, MatMenuModule
+} from '@angular/material';
+import { EditorComponent } from './editor/editor.component';
+
+const appRoutes: Routes = [
+  { path: 'editor', component: EditorComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    EditorComponent
   ],
   imports: [
-    BrowserModule
+    DragDropModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    FlexLayoutModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    MatButtonToggleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/editor/editor.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Cell {
+  value?: string;
+  color?: string;
+  constructor(public id: number) { }
+}
+
+@Component({
+  selector: 'app-editor',
+  templateUrl: './editor.component.html',
+  styleUrls: ['./editor.component.scss']
+})
+export class EditorComponent implements OnInit {
+
+  public cells: Cell[] = [];
+
+  constructor() {
+    this.reload();
+  }
+
+  ngOnInit() {
+  }
+
+  reload() {
+    this.cells = [];
+    for (let index = 0; index < 100; index++) {
+      this.cells.push(new Cell(index));
+    }
+  }
+
+}
